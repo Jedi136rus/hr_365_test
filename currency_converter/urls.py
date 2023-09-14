8e311cba2864ebb972eb4ab4363cd12cf6a5cbbf
Петр Чайкин <p.chaikin@numedy.com>
2023-09-14 18:07:48 +0300

    save_changes

--- currency_converter/urls.py
+++ currency_converter/urls.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import views
+
+API_KEY = 'cur_live_XKf0Vm1furR8r1Q8WolbFzwqEzKv6bRrCuuaRvlO'  # убрать потом в переменные докера
 
 urlpatterns = [
+    path("convert_value/", views.convert_value),
 ]
